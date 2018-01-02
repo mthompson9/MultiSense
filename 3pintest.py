dd0aba3b61e44a734f14d46d3c2237ac7c20ca42
mthompson9 <32095085+mthompson9@users.noreply.github.com>
2018-01-02 14:28:12 +0000

    Add files via upload

--- 3pintest.py
+++ 3pintest.py
@@ -2,51 +2,58 @@ import RPi.GPIO as GPIO
 
 import time
 
+GPIO.setwarnings(False)
+
 
 
 TRIG = 23
 
 ECHO = 24
 
-for i in range (1, 10):
-
-    print ("Distance Measurement In Progress")
+def distChecker ():
     
-    GPIO.setmode(GPIO.BCM)
+    for i in range (1, 10):
 
-    GPIO.setup(TRIG,GPIO.OUT)
-
-    GPIO.setup(ECHO,GPIO.IN)
-
-    GPIO.output(TRIG, False)
-
-    print ("Waiting For Sensor To Settle")
-
-    time.sleep(2)
-
-    GPIO.output(TRIG, True)
-
-    time.sleep(0.00001)
-
-    GPIO.output(TRIG, False)
-
-    while GPIO.input(ECHO)==0:
-
-      pulse_start = time.time()
+##    print ("Distance Measurement In Progress")
     
-    while GPIO.input(ECHO)==1:
+        GPIO.setmode(GPIO.BCM)
 
-      pulse_end = time.time()
+        GPIO.setup(TRIG,GPIO.OUT)
+
+        GPIO.setup(ECHO,GPIO.IN)
+
+        GPIO.output(TRIG, False)
+
+##    print ("Waiting For Sensor To Settle")
+
+        time.sleep(2)
+
+        GPIO.output(TRIG, True)
+
+        time.sleep(0.00001)
+
+        GPIO.output(TRIG, False)
+
+        while GPIO.input(ECHO)==0:
+
+          pulse_start = time.time()
+    
+        while GPIO.input(ECHO)==1:
+
+          pulse_end = time.time()
   
-    pulse_duration = pulse_end - pulse_start
+        pulse_duration = pulse_end - pulse_start
 
-    distance = pulse_duration*17150
+        distance = pulse_duration*17150
 
-    distance = round(distance, 2)
+        distance = round(distance, 2)
+        
+        result = ("Distance:"),distance,("cm")
 
-    print (("Distance:"),distance,("cm"))
-
-    GPIO.cleanup()
-
-
+        GPIO.cleanup()
+        
+        return result
+    
+while True:
+    print (distChecker())
   
